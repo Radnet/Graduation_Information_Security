0d4d275b999742ca4a3a5b9b75434c9012b2b32b
Rafael Adnet <rafaadnet@gmail.com>
2015-05-04 11:34:30 -0300

    Main Adjust

--- Trab3_Grupo3/src/Main.java
+++ Trab3_Grupo3/src/Main.java
@@ -3,18 +3,26 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		FrameLogin FrameLogin = new FrameLogin("Seja bem-vindo ao Trab3 de segurança!");
+		FrameLogin FrameLogin = new FrameLogin("Trab3 Segurança | Etapa 1 - Login");
 		FrameLogin.setVisible(true);
+		
 		
 		//******************************************************************
 		//TESTE
+
+		/*
+		 
 		User user = User.GetUserObj();
 		user.setLogin("admin");
 		
 		SharedLibrary.FillUserProps("admin");
 		
 		FrameMenu frameMenu = new FrameMenu("Frame Menu");
-		//frameMenu.setVisible(true);
+		
+		frameMenu.setVisible(true);
+		
+		*/
 		//**********************************************
+
 	}
 }
